2480a7d3170a4483a4e8aa85455855d8715fa4d5
Joan Fontanals Martinez <joan.martinez@jina.ai>
2022-06-02 17:06:48 +0200

    small printing

--- src/train/dual_distillation/train.py
+++ src/train/dual_distillation/train.py
@@ -168,7 +168,14 @@ def run_evaluations(image_encoder, text_encoder, text_tokenizer, vilt_model, bat
                     pixel_bert_transformed_images.append(vilt_transform(i).to(device))
                     image_tensors.append(dual_encoder_transform(i))
 
-                images_embeddings = image_encoder(torch.stack(image_tensors).to(device))
+                print(f' len of image_tensors {image_tensors}')
+                deviced_tensors = torch.stack(image_tensors).to(device)
+                try:
+                    print(f' deviced tensors device {deviced_tensors.get_device()}')
+                except:
+                    pass
+
+                images_embeddings = image_encoder(deviced_tensors)
                 all_image_embeddings.append(images_embeddings)
                 image_filenames.extend(filenames)
                 all_images_ids.extend(image_ids)
